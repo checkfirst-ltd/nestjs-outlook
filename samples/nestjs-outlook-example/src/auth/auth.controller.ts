041f04726ba810f7149922cc09dd379e85b30e14
Rami ELSAWY <ramielsawy@gmail.com>
2025-05-10 04:03:20 +0300

    chore: Example of using custom scopes

--- samples/nestjs-outlook-example/src/auth/auth.controller.ts
+++ samples/nestjs-outlook-example/src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Req } from '@nestjs/common';
-import { MicrosoftAuthService } from '@checkfirst/nestjs-outlook';
+import { MicrosoftAuthService, PermissionScope } from '@checkfirst/nestjs-outlook';
 
 @Controller('auth')
 export class AuthController {
@@ -12,6 +12,14 @@ export class AuthController {
     // In a real application, you would have proper authentication
     // For this example, we're using a mock user ID
     const mockUserId = '1';
-    return await this.microsoftAuthService.getLoginUrl(mockUserId);
+    
+    // Pass the permission scopes to the login URL. They are defined in PermissionScope enum
+    return await this.microsoftAuthService.getLoginUrl(mockUserId, [
+      PermissionScope.CALENDAR_READ,
+      PermissionScope.CALENDAR_WRITE,
+      PermissionScope.EMAIL_READ,
+      PermissionScope.EMAIL_WRITE,
+      PermissionScope.EMAIL_SEND,
+    ]);
   }
 } 
